696a3ef289ca1a42b81d6851c2760beab1a59e48
Takamasa Sugiura <takmasas2008@gmail.com>
2023-06-03 18:59:30 +0900

    fix clock parameter

--- ClockEffect.js
+++ ClockEffect.js
@@ -1,13 +1,25 @@
 class ClockEffect {
-    direction = SoDirection.BottomRight;
+    direction = "BottomRight";
     margin = 10;
+    size = 48;
+    color = "white";
 
-    constructor(direction) {
-        if (direction instanceof SoDirection) {
-            this.direction = direction;
+    constructor(parameter) {
+        if (typeof parameter === "string") {
+            this.direction = parameter;
+            return;
         }
-        else if (direction !== undefined) {
-            this.direction = new SoDirection(direction);
+        if (parameter.direction !== undefined) {
+            this.direction = parameter.direction;
+        }
+        if (parameter.margin !== undefined) {
+            this.margin = parameter.margin;
+        }
+        if (parameter.size !== undefined) {
+            this.size = parameter.size;
+        }
+        if (parameter.color !== undefined) {
+            this.color = parameter.color;
         }
     }
 
@@ -17,49 +29,50 @@ class ClockEffect {
         const secondSource = "00" + today.getSeconds();
         const minuteSource = "00" + today.getMinutes();
         const time = today.getHours() + ":" + minuteSource.substring(minuteSource.length - 2, minuteSource.length) + ":" + secondSource.substring(secondSource.length - 2, secondSource.length);
-        ctx.font = "48px sans"
-        ctx.fillStyle = "white";
-        if (this.direction.name == "TopLeft") {
+        ctx.font = this.size + "px sans"
+        ctx.fillStyle = this.color;
+        const direction = this.direction.toUpperCase();
+        if (direction == "TOPLEFT") {
             ctx.textBaseline = "top";
             ctx.textAlign = "left";
             ctx.fillText(time, this.margin, this.margin);
         }
-        else if (this.direction.name === "Top") {
+        else if (direction === "TOP") {
             ctx.textBaseline = "top";
             ctx.textAlign = "center";
             ctx.fillText(time, data.canvasRect.w / 2, this.margin);
         }
-        else if (this.direction.name === "TopRight") {
+        else if (direction === "TOPRIGHT") {
             ctx.textBaseline = "top";
             ctx.textAlign = "right";
             ctx.fillText(time, data.canvasRect.w - this.margin, this.margin);
         }
-        else if (this.direction.name === "Left") {
+        else if (direction === "LEFT") {
             ctx.textBaseline = "middle";
             ctx.textAlign = "left";
             ctx.fillText(time, this.margin, data.canvasRect.h / 2);
         }
-        else if (this.direction.name === "Center") {
+        else if (direction === "CENTER") {
             ctx.textBaseline = "middle";
             ctx.textAlign = "center";
             ctx.fillText(time, data.canvasRect.w / 2, data.canvasRect.h / 2);
         }
-        else if (this.direction.name === "Right") {
+        else if (direction === "RIGHT") {
             ctx.textBaseline = "middle";
             ctx.textAlign = "right";
             ctx.fillText(time, data.canvasRect.w - this.margin, data.canvasRect.h / 2);
         }
-        else if (this.direction.name === "BottomLeft") {
+        else if (direction === "BOTTOMLEFT") {
             ctx.textBaseline = "bottom";
             ctx.textAlign = "left";
             ctx.fillText(time, this.margin, data.canvasRect.h - this.margin);
         }
-        else if (this.direction.name === "Bottom") {
+        else if (direction === "BOTTOM") {
             ctx.textBaseline = "bottom";
             ctx.textAlign = "center";
             ctx.fillText(time, data.canvasRect.w / 2, data.canvasRect.h - this.margin);
         }
-        else if (this.direction.name === "BottomRight") {
+        else {
             ctx.textBaseline = "bottom";
             ctx.textAlign = "right";
             ctx.fillText(time, data.canvasRect.w - this.margin, data.canvasRect.h - this.margin);
